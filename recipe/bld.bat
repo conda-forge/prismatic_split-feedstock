322a5287e08f27291464f5e037db26ef04c19a73
Eric Prestat <eric.prestat@gmail.com>
2020-03-08 16:55:11 +0000

    Fix issue with finding fftw on windows: cmake patch.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,13 +1,16 @@
+setlocal EnableDelayedExpansion
+
 :: Make a build folder and change to it.
 mkdir build && cd build
-
-set HDF5_DIR=%LIBRARY_PREFIX%\cmake\hdf5
 
 :: Configure using the CMakeFiles
 cmake -G "NMake Makefiles" ^
       -D PRISMATIC_ENABLE_CLI=1 ^
       -D PRISMATIC_ENABLE_GUI=1 ^
       -D PRISMATIC_ENABLE_GPU=0 ^
+      -D PRISMATIC_ENABLE_DOUBLE_PRECISION=1 ^
+      -D HDF5_DIR=%LIBRARY_PREFIX%\cmake\hdf5 ^
+      -D Qt5Widgets_DIR=%LIBRARY_PREFIX%\lib\cmake\Qt5Widgets ^
       -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
       -D CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
       -D CMAKE_BUILD_TYPE=Release ^
