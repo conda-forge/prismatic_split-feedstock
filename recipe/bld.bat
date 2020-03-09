fa9008e75c5d94e3ba7a5eb35a75dff163dfe67e
Eric Prestat <eric.prestat@gmail.com>
2020-03-09 16:24:55 +0000

    Set gui and cli argument to windows build.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,12 +3,18 @@ setlocal EnableDelayedExpansion
 :: Make a build folder and change to it.
 mkdir build && cd build
 
+if "%1"=="cli" (set cli=1) else (set cli=0)
+
+if "%1"=="gui" (set gui=1) else (set gui=0)
+
+echo "cli is" %cli% "and gui is" %gui%
+
 :: Configure using the CMakeFiles
 cmake -G "NMake Makefiles" ^
-      -D PRISMATIC_ENABLE_CLI=1 ^
-      -D PRISMATIC_ENABLE_GUI=1 ^
+      -D PRISMATIC_ENABLE_CLI=%cli% ^
+      -D PRISMATIC_ENABLE_GUI=%gui% ^
       -D PRISMATIC_ENABLE_GPU=0 ^
-      -D PRISMATIC_ENABLE_DOUBLE_PRECISION=1 ^
+      -D PRISMATIC_ENABLE_DOUBLE_PRECISION=0 ^
       -D HDF5_DIR=%LIBRARY_PREFIX%\cmake\hdf5 ^
       -D Qt5Widgets_DIR=%LIBRARY_PREFIX%\lib\cmake\Qt5Widgets ^
       -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
